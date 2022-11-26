e82391382d6d957f4699f1d5aaa58c32f30fe75b
ashish4308 <ashish.rawat4308@gmail.com>
2022-11-26 12:09:42 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Homepage from './compo/Homepage'
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Homepage/> */}
+      chup ho jo
+      
+    </div>
+  );
+}
+
+export default App;
